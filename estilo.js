28b194774379d72304c07a27bd6a103819eedad1
Biel2.0 <125614460+Bobiel@users.noreply.github.com>
2024-10-26 18:32:42 -0300

    at15

--- estilo.js
+++ estilo.js
@@ -41,9 +41,11 @@
           let menuMobile = document.querySelector(".mobile-menu");
           if (menuMobile.classList.contains("open")) {
             menuMobile.classList.remove("open");
-            document.querySelector('.icon').src= "x-solid.svg"
+            document.querySelector('.icon').src= " ";
           }
-           else {
+          else {
             menuMobile.classList.add("open");
+            document.querySelector('.icon').src= "x-solid.svg";
+
           }
         }
